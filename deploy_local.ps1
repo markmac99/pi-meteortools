2066d3a3762a89d9117b3fe93bbd1da217298725
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-03-28 13:31:53 +0100

    fix small bug

--- deploy_local.ps1
+++ deploy_local.ps1
@@ -1,7 +1,7 @@
 # script to deploy the Windows scripts locally on my PC
 set-location $PSScriptRoot
 # load the helper functions
-. .\DailyChecks\helperfunctions.ps1
+. .\Windows\helperfunctions.ps1
 # read the inifile
 $ini=get-inicontent 'windows\UK0006.ini'
 $targ=$ini['camera']['localfolder']+'\..\scripts\'
@@ -10,6 +10,7 @@ $targ=$targ.replace('/','\')
 xcopy /dy /exclude:exclude.rsp .\Windows\*.ps1 $targ
 xcopy /dy /exclude:exclude.rsp .\Windows\*.ini $targ
 
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\DailyChecks\*.ps1 $targ
 xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\*.py $targ
 xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\*.ps1 $targ
 xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\FormatConverters\*.py $targ
@@ -22,5 +23,5 @@ xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\CameraCurator\*.py $t
 if ((test-path $targ\UFOHandler) -eq 0 ){ mkdir $targ\UFOHandler }
 xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\UFOHandler\*.py $targ\UFOHandler
 
-xcopy /dy .\Pi\sunwait-src\sunwait.exe $targ
+#xcopy /dy .\Pi\sunwait-src\sunwait.exe $targ
 xcopy /dy ..\ukmon-shared\UKMonCPPTools\stacker\build\Release\stacker.exe $targ
