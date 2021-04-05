168fbb907bf7822f30077cc57455fe0e875b7308
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-04-05 14:39:02 +0100

    updated file locations

--- deploy_local.ps1
+++ deploy_local.ps1
@@ -11,17 +11,17 @@ xcopy /dy /exclude:exclude.rsp .\Windows\*.ps1 $targ
 xcopy /dy /exclude:exclude.rsp .\Windows\*.ini $targ
 
 xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\DailyChecks\*.ps1 $targ
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\*.py $targ
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\*.ps1 $targ
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\FormatConverters\*.py $targ
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\orbitSolver\*.py $targ
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\orbitSolver\*.ps1 $targ
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\*.py $targ
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\*.ps1 $targ
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\FormatConverters\*.py $targ
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\orbitSolver\*.py $targ
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\orbitSolver\*.ps1 $targ
 
 if ((test-path $targ\CameraCurator) -eq 0 ){ mkdir $targ\CameraCurator }
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\CameraCurator\*.py $targ\CameraCurator
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\CameraCurator\*.py $targ\CameraCurator
 
 if ((test-path $targ\UFOHandler) -eq 0 ){ mkdir $targ\UFOHandler }
-xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\NewAnalysis\UFOHandler\*.py $targ\UFOHandler
+xcopy /dy /exclude:exclude.rsp ..\ukmon-shared\analysis\UFOHandler\*.py $targ\UFOHandler
 
 #xcopy /dy .\Pi\sunwait-src\sunwait.exe $targ
 xcopy /dy ..\ukmon-shared\UKMonCPPTools\stacker\build\Release\stacker.exe $targ
