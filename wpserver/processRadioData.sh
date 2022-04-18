eeb7d7d343ed792c9b3b45f89f8e77d9bcf3ed5d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-04-18 15:46:31 +0100

    updating to remove stuff done via lambda

--- wpserver/processRadioData.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source $here/config.ini
-
-source ~/venvs/wmpl/bin/activate
-cd $here
-ym=$(date +%Y%m)
-python $here/lambdaForRadio.py ~/data/rawradiodata/ ~/data/Radio/ ${ym}
