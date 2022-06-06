c9d7d1b07263f2dc239a95b236d72c3538233056
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-06 13:34:45 +0100

    add various missing tags

--- wpserver/lambdas/add_various_tags.ps1
+++ wpserver/lambdas/add_various_tags.ps1
@@ -6,3 +6,7 @@ aws cloudwatch tag-resource --resource-arn "arn:aws:cloudwatch:eu-west-2:3179762
 aws cloudwatch tag-resource --resource-arn "arn:aws:cloudwatch:eu-west-2:317976261112:alarm:ukmonHelperDiskspace" --tags Key=billingtag,Value=ukmon --region eu-west-2
 aws cloudwatch tag-resource --resource-arn "arn:aws:cloudwatch:eu-west-2:317976261112:alarm:awsec2-i-0da38ed8aea1a1d85-LessThanOrEqualToThreshold-CPUUtilization" --tags Key=billingtag,Value=ukmon --region eu-west-2
 aws cloudwatch tag-resource --resource-arn "arn:aws:cloudwatch:eu-west-2:317976261112:alarm:Wordpress Diskspace Alert" --tags Key=billingtag,Value=MarysWebsite --region eu-west-2
+
+aws cloudformation update-stack --stack-name createCamIndexes --use-previous-template --capabilities CAPABILITY_IAM --tags Key=billingtag,Value=MarksWebsite --region eu-west-2
+aws cloudformation update-stack --stack-name processRadioData --use-previous-template --capabilities CAPABILITY_IAM --tags Key=billingtag,Value=MarksWebsite --region eu-west-2
+aws cloudformation update-stack --stack-name processRadioJpgs --use-previous-template --capabilities CAPABILITY_IAM --tags Key=billingtag,Value=MarksWebsite --region eu-west-2
