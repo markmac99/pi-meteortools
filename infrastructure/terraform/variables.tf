e1c9a86a2af36cd2fcfdae4b78b8449ed030ca5d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-13 17:39:23 +0100

    adding relevant infra

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "profile" {
+    description = "AWS creds to use"
+    default = "default"
+}
+variable "region" {
+        default = "eu-west-2"
+}
+
+variable "access_key" {
+    description = "Access Key"
+    default = ""
+}
+
+variable "secret_key" {
+    description = "Secret Key"
+    default = ""
+}
+
