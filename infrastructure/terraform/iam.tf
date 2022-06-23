436f355006834e117369197583a253843bfb7eba
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-06-23 20:56:06 +0100

    add s3user terraform

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -1,3 +1,79 @@
+# User, Policy and Roles used to mount s3 buckets. 
+resource "aws_iam_user" "s3user" {
+  name = "s3user"
+  tags = {
+    "billingtag" = "MarksWebsite"
+  }
+}
+
+resource "aws_iam_policy" "MMS3BucketAccessRW" {
+  name        = "MMS3BucketAccessRW"
+  policy      = data.aws_iam_policy_document.MMS3BucketAccessRW-policy-doc.json
+  description = "Access to MM S3 Buckets"
+  tags = {
+    "billingtag" = "MarksWebsite"
+  }
+}
+
+data "aws_iam_policy_document" "MMS3BucketAccessRW-policy-doc" {
+  statement {
+    actions = ["s3:ListBucket"]
+    effect  = "Allow"
+    resources = [
+      "arn:aws:s3:::mlm-website-backups",
+      "arn:aws:s3:::mjmm-website-backups",
+      "arn:aws:s3:::mjmm-meteor-uploads",
+      "arn:aws:s3:::mjmm-data",
+    ]
+    sid = "VisualEditor0"
+  }
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+    effect = "Allow"
+    resources = [
+      "arn:aws:s3:::mjmm-data/*",
+      "arn:aws:s3:::mjmm-meteor-uploads/*",
+      "arn:aws:s3:::mlm-website-backups/*",
+      "arn:aws:s3:::mjmm-website-backups/*",
+    ]
+    sid = "VisualEditor1"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "s3user-pol-attachment" {
+  user       = aws_iam_user.s3user.name
+  policy_arn = "arn:aws:iam::317976261112:policy/MMS3BucketAccessRW"
+}
+
+#inline policy used by s3user
+resource "aws_iam_user_policy" "Ukmon-shared-access" {
+  name = "Ukmon-shared-access"
+  user = aws_iam_user.s3user.name
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+          ]
+          Effect = "Allow"
+          Resource = [
+            "arn:aws:s3:::ukmon-shared/*",
+            "arn:aws:s3:::ukmon-live/*",
+            "arn:aws:s3:::ukmeteornetworkarchive/*",
+          ]
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+}
 
 # role for SSM hybrid device management
 resource "aws_iam_role" "hybrid_ssm_role" {
