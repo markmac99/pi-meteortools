15a7198de94f96bd340d953a155e886ee95cee70
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-08-03 12:07:50 +0100

    add useful logging

--- windows/testml.ps1
+++ windows/testml.ps1
@@ -1,13 +1,15 @@
 # test the ML routine
 
+Write-Output "starting" > F:\videos\MeteorCam\logs\testml.log
+
 $dirs = (Get-ChildItem F:\videos\MeteorCam\UK0006\ArchivedFiles\UK0006_202207*).fullname
-foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir} > F:\videos\MeteorCam\logs\testml.log
+foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir >> F:\videos\MeteorCam\logs\testml.log } 
 
 $dirs = (Get-ChildItem F:\videos\MeteorCam\UK000F\ArchivedFiles\UK000F_202207*).fullname
-foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir} >> F:\videos\MeteorCam\logs\testml.log
+foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir >> F:\videos\MeteorCam\logs\testml.log }
 
 $dirs = (Get-ChildItem F:\videos\MeteorCam\UK001L\ArchivedFiles\UK001L_202207*).fullname
-foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir} >> F:\videos\MeteorCam\logs\testml.log
+foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir >> F:\videos\MeteorCam\logs\testml.log }
 
 $dirs = (Get-ChildItem F:\videos\MeteorCam\UK002F\ArchivedFiles\UK002F_202207*).fullname
-foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir} >> F:\videos\MeteorCam\logs\testml.log
+foreach ($dir in $dirs) { python -m utils.compareMLtoManual $dir >> F:\videos\MeteorCam\logs\testml.log }
