6fc4fdf0348db94200746e89d35509ca290e7fd4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-09-26 02:03:28 +0100

    Suport for startrails

--- wpserver/createCamIndex.sh
+++ wpserver/createCamIndex.sh
@@ -30,7 +30,11 @@ for mth in $mthlist ; do
             if compgen -G "$cam/videos/$mth" > /dev/null ; then 
                 echo "cell.innerHTML = \"\\<a href=\\\"/data/mjmm-data/$cam/videos/$mth\\\"\\>$mth\\</a\\>\";" >> $idxfile
             else
-                echo "cell.innerHTML = \"\";" >> $idxfile
+                if [ "$mth" == "trackstacks" ] ; then
+                    echo "cell.innerHTML = \"\\<a href=\\\"/data/mjmm-data/$cam/startrails\\\"\\>startrails\\</a\\>\";" >> $idxfile
+                else
+                    echo "cell.innerHTML = \"\";" >> $idxfile
+                fi 
             fi
         else
             if compgen -G "$cam/$mth" > /dev/null ; then 
@@ -65,6 +69,43 @@ if [ $? -gt 0 ] ; then
 else
     echo nothing changed
 fi
+
+idxfile=$TMPDIR/cameraindex.js
+echo "\$(function() {" > $idxfile
+echo "var table = document.createElement(\"table\");" >> $idxfile
+echo "table.className = \"table table-striped table-bordered table-hover table-condensed\";" >> $idxfile
+echo "var header = table.createTHead(); " >> $idxfile
+echo "header.className = \"h4\"; " >> $idxfile
+
+mthlist=$(ls -1dr allsky/startrails/202* | awk -F/ '{print $3}')
+if [[ $mthlist != *"$currmth"* ]] ; then mthlist=$(echo $currmth $mthlist ) ; fi
+
+echo "var row = table.insertRow(-1);" >> $idxfile
+i=0
+for mth in $mthlist ; do 
+    if [ $i -eq 5 ] ; then
+        echo "var row = table.insertRow(-1);" >> $idxfile
+        i=0
+    fi
+    echo "var cell = row.insertCell(-1);" >> $idxfile
+    if compgen -G "allsky/startrails/$mth" > /dev/null ; then 
+        echo "cell.innerHTML = \"\\<a href=\\\"/data/mjmm-data/allsky/startrails/$mth\\\"\\>$mth\\</a\\>\";" >> $idxfile
+    fi
+    i=$((i+1))
+done
+
+echo "var outer_div = document.getElementById(\"mthindex\");"   >> $idxfile
+echo "outer_div.appendChild(table);"  >> $idxfile
+echo "})"  >> $idxfile
+
+diff $idxfile $DATADIR/allsky/startrails/cameraindex.js > /dev/null 
+if [ $? -gt 0 ] ; then
+    echo deploying changes
+    cp $idxfile $DATADIR/allsky/startrails/cameraindex.js
+else
+    echo nothing changed
+fi
+
 
 export PYLIB=~/prod/ukmon_pylib
 source ~/venvs/wmpl/bin/activate
