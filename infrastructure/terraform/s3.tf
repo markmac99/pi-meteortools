0c04d9355405372b0eb0239abe6dda9436d4730b
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2022-11-27 20:38:51 +0000

    add bucket management

---
+++ infrastructure/terraform/s3.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "mjmmauditing" {
+  force_destroy = false
+  bucket        = "mjmmauditing"
+  tags = {
+    "billingtag" = "Management"
+  }
+  object_lock_enabled = false
+}
+
+resource "aws_s3_bucket" "mjmm-data" {
+  bucket = "mjmm-data"
+  tags = {
+    "billingtag" = "MarksWebsite"
+  }
+  object_lock_enabled = false
+}
+
+resource "aws_s3_bucket_logging" "ukmslogging" {
+  bucket = aws_s3_bucket.mjmm-data.id
+
+  target_bucket = aws_s3_bucket.mjmmauditing.id
+  target_prefix = "data/"
+}
+
+data "aws_s3_bucket" "rawradiodata" {
+  bucket = "mjmm-rawradiodata"
+}
+
+resource "aws_s3_bucket_logging" "rrdlogging" {
+  bucket = data.aws_s3_bucket.rawradiodata.id
+
+  target_bucket = aws_s3_bucket.mjmmauditing.id
+  target_prefix = "rawradiodata/"
+}
+
+data "aws_s3_bucket" "rawsatdata" {
+  bucket = "mjmm-rawsatdata"
+}
+
+resource "aws_s3_bucket_logging" "rsdlogging" {
+  bucket = data.aws_s3_bucket.rawsatdata.id
+
+  target_bucket = aws_s3_bucket.mjmmauditing.id
+  target_prefix = "rawsatdata/"
+}
