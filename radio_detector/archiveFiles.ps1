1dc6967a8c8fe2a5bb9d07069bc5b888b6284f2e
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-03-01 22:20:16 +0000

    improvements to radio archiving

--- radio_detector/archiveFiles.ps1
+++ radio_detector/archiveFiles.ps1
@@ -15,7 +15,7 @@ if((test-path $inifname) -eq $false){
     write-output "datadir missing or invalid, can't continue"
     exit 2
 }
-$arcdir=($ini['host']['archivedir']).replace('/','\')
+$arcdir=($ini['host']['archivedir'])
 Set-Location $datadir
 $txtdt=[string]$args[0]
 #$jpgdt=$txtdt.substring(2)
@@ -24,13 +24,27 @@ $jpgdt=$txtdt
 compress-archive -path sounds\event$txtdt*.wav -DestinationPath sounds\sound_$txtdt.zip
 if ((test-path sounds\sound_$txtdt.zip) -eq 1)
 {
-    Move-Item sounds\sound_$txtdt.zip $arcdir -force
+    if ($arcdir.substring(0,5) -eq "s3://")
+    {
+        aws s3 mv sounds\sound_$jpgdt.zip $arcdir
+    }
+    else
+    {
+        Move-Item sounds\sound_$jpgdt.zip $arcdir -force
+    }
     Remove-Item sounds\event$txtdt*.wav
 }
 compress-archive -path screenshots\event$jpgdt*.jpg -DestinationPath screenshots\images_$jpgdt.zip
 if ((test-path screenshots\images_$jpgdt.zip) -eq 1)
 {
-    Move-Item screenshots\images_$jpgdt.zip $arcdir -force
+    if ($arcdir.substring(0,5) -eq "s3://")
+    {
+        aws s3 mv screenshots\images_$jpgdt.zip $arcdir
+    }
+    else
+    {
+        Move-Item screenshots\images_$jpgdt.zip $arcdir -force
+    }
     Remove-Item screenshots\event$jpgdt*.jpg
 }
 set-location $curloc
