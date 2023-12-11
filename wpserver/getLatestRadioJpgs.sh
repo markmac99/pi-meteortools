429dac24233282203139d358eda9bf4247dd2926
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-11 21:20:03 +0000

    performance improvements

--- wpserver/getLatestRadioJpgs.sh
+++ wpserver/getLatestRadioJpgs.sh
@@ -2,13 +2,19 @@
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/config.ini
+active=$(ps -ef | grep getLatestRadioJpgs | grep -v grep | awk '{print $2}')
+if [ "$active" == "" ] ; then 
+	cd $DATADIR/Radio
+	#aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "*latest.jpg" --include "screenshot*.jpg"
+	aws s3 cp s3://mjmm-data/Radio/RMOB_latest.jpg .
+	aws s3 cp s3://mjmm-data/Radio/3months_latest.jpg .
+	aws s3 cp s3://mjmm-data/Radio/screenshot1.jpg .
+	aws s3 cp s3://mjmm-data/Radio/screenshot2.jpg .
 
-cd $DATADIR/Radio
-aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "*latest.jpg" --include "screenshot*.jpg"
-
-cd $DATADIR/
-camlist=$(ls -1d UK* allsky/startrails allsky/videos)
-for cam in $camlist ; do 
-    mkdir -p $cam/$currmth 
-    aws s3 sync s3://mjmm-data/$cam/ ./$cam --exclude "*" --include "*.js" --include "*.html"
-done
+	#cd $DATADIR/
+	#camlist=$(ls -1d UK* allsky/startrails allsky/videos)
+	#for cam in $camlist ; do 
+	#    mkdir -p $cam/$currmth 
+	#    aws s3 sync s3://mjmm-data/$cam/ ./$cam --exclude "*" --include "*.js" --include "*.html"
+	#done
+fi 
