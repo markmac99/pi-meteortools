a9474e2323c94f87846b5c95f9a7c9a2d423da16
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-12-13 22:40:49 +0000

    bugfix: didn't properly check if already running

--- wpserver/getLatestRadioJpgs.sh
+++ wpserver/getLatestRadioJpgs.sh
@@ -2,19 +2,15 @@
 
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $here/config.ini
-active=$(ps -ef | grep getLatestRadioJpgs | grep -v grep | awk '{print $2}')
-if [ "$active" == "" ] ; then 
+if [ ! -f /tmp/getLatestRadioJpgs.running ] ; then 
+	echo "1" > /tmp/getLatestRadioJpgs.running
 	cd $DATADIR/Radio
 	#aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "*latest.jpg" --include "screenshot*.jpg"
 	aws s3 cp s3://mjmm-data/Radio/RMOB_latest.jpg .
 	aws s3 cp s3://mjmm-data/Radio/3months_latest.jpg .
 	aws s3 cp s3://mjmm-data/Radio/screenshot1.jpg .
 	aws s3 cp s3://mjmm-data/Radio/screenshot2.jpg .
-
-	#cd $DATADIR/
-	#camlist=$(ls -1d UK* allsky/startrails allsky/videos)
-	#for cam in $camlist ; do 
-	#    mkdir -p $cam/$currmth 
-	#    aws s3 sync s3://mjmm-data/$cam/ ./$cam --exclude "*" --include "*.js" --include "*.html"
-	#done
+	rm -f /tmp/getLatestRadioJpgs.running
+else
+	echo "already running"
 fi 
