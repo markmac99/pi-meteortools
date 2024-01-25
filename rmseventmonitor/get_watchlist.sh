64bf925fcc05f5bab9429b9cb5eb2729fa26fd97
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-01-25 21:25:27 +0000

    remove unused files

--- rmseventmonitor/get_watchlist.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp gmn.uwo.ca:/var/www/html/globalmeteornetwork.org/events/event_watchlist.txt .
--- rmseventmonitor/put_watchlist.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp event_watchlist.txt gmn.uwo.ca:/var/www/html/globalmeteornetwork.org/events/
