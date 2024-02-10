d87afc5cf8a7c5c996df2b4c690796ed19535e37
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-10 23:30:21 +0000

    get and put the inf file

--- auroracam/archiveData.sh
+++ auroracam/archiveData.sh
@@ -7,5 +7,6 @@ source $here/config.ini > /dev/null 2>&1
 source ~/vAuroracam/bin/activate
 
 cd $DATADIR
-
+aws s3 cp s3://mjmm-data/auroracam/FILES_TO_UPLOAD.inf ~/RMS_data/auroracam/
 python $here/archAndFree.py
+aws s3 cp ~/RMS_data/auroracam/FILES_TO_UPLOAD.inf  s3://mjmm-data/auroracam/
