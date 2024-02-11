fec97383dac52cb4033a96d63a05af9b0a8d21f1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-02-11 16:39:54 +0000

    purge logs too

--- auroracam/archiveData.sh
+++ auroracam/archiveData.sh
@@ -10,3 +10,6 @@ cd $DATADIR
 aws s3 cp s3://mjmm-data/auroracam/FILES_TO_UPLOAD.inf ~/RMS_data/auroracam/
 python $here/archAndFree.py
 aws s3 cp ~/RMS_data/auroracam/FILES_TO_UPLOAD.inf  s3://mjmm-data/auroracam/
+
+cd $DATADIR/../logs
+find . -mtime +30 -exec rm -f {} \;
