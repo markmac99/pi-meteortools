e7c55a76333e47ddc611b0ee629ca3a8528acb24
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-04-22 21:38:28 +0100

    remove unused stuff

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -1,20 +1,3 @@
-# Copyright (C) Mark McIntyre
-data "aws_security_group" "launch-wizard-4" {
-  id = "sg-0b7fab84089059501"
-}
-/*
-resource "aws_instance" "adminserver" {
-  ami                  = "ami-03e88be9ecff64781"
-  instance_type        = "t3a.nano"
-  iam_instance_profile = aws_iam_instance_profile.cwagentadminprofile.name
-  key_name             = data.aws_key_pair.marks_key2.key_name
-  security_groups      = [data.aws_security_group.launch-wizard-4.name]
-  tags = {
-    "Name"       = "admin"
-    "billingtag" = "Management"
-  }
-}
-*/
 # ssh key - this is created  by the UKMON stuff
 data "aws_key_pair" "marks_key2" {
   key_name           = "marks_key2"
