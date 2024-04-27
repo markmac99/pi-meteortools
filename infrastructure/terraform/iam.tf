d0ba6f58ea3192fa80c534c6eab7d94872fa29c7
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-04-27 23:06:04 +0100

    add policy and user for allsky uploads

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -7,6 +7,13 @@ resource "aws_iam_user" "s3user" {
   }
 }
 
+resource "aws_iam_user" "allsky" {
+  name = "allsky"
+  tags = {
+    "billingtag" = "MarksWebsite"
+  }
+}
+
 resource "aws_iam_policy" "MMS3BucketAccessRW" {
   name        = "MMS3BucketAccessRW"
   policy      = data.aws_iam_policy_document.MMS3BucketAccessRW-policy-doc.json
@@ -49,7 +56,7 @@ data "aws_iam_policy_document" "MMS3BucketAccessRW-policy-doc" {
 
 resource "aws_iam_user_policy_attachment" "s3user-pol-attachment" {
   user       = aws_iam_user.s3user.name
-  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/MMS3BucketAccessRW"
+  policy_arn = aws_iam_policy.MMS3BucketAccessRW.arn
 }
 
 # role for SSM hybrid device management
@@ -92,5 +99,40 @@ resource "aws_ssm_activation" "ssm_local_activation" {
   }
 }
 
-output "activation_id" { value = aws_ssm_activation.ssm_local_activation.id }
-output "activationcode" { value = aws_ssm_activation.ssm_local_activation.activation_code }
+resource "aws_iam_policy" "allsky-policy" {
+  name        = "s3AccessForAllsky"
+  policy      = data.aws_iam_policy_document.allsky-policy-doc.json
+  description = "Access to MM S3 Buckets"
+  tags = {
+    "billingtag" = "MarksWebsite"
+  }
+}
+
+data "aws_iam_policy_document" "allsky-policy-doc" {
+  statement {
+    actions = ["s3:ListBucket"]
+    effect  = "Allow"
+    resources = [
+      "arn:aws:s3:::${var.databucket}"
+    ]
+    sid = "VisualEditor0"
+  }
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+    effect = "Allow"
+    resources = [
+      "arn:aws:s3:::${var.databucket}/*"
+    ]
+    sid = "VisualEditor1"
+  }
+}
+
+resource "aws_iam_user_policy_attachment" "allsky-pol-attachment" {
+  user       = aws_iam_user.allsky.name
+  policy_arn = aws_iam_policy.allsky-policy.arn
+}
+
