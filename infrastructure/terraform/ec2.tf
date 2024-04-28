7cfed5ae4f92807ada4bc40c5b266d203f1ca0f4
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-04-29 00:04:09 +0100

    consolidating non-ukmon Terraform

--- infrastructure/terraform/ec2.tf
+++
@@ -1,9 +0,0 @@
-# ssh key - this is created  by the UKMON stuff
-data "aws_key_pair" "marks_key2" {
-  key_name           = "marks_key2"
-  #include_public_key = True
-  #filter {
-  #  name   = "Name"
-  #  values = ["marks_key2"]
-  #}
-}
