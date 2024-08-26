475519535f7fe9c7aec793c47098906cf5abd9ef
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-08-26 18:34:34 +0100

    Small improvements to auroracam software

--- auroracam/startAuroraCam.sh
+++ auroracam/startAuroraCam.sh
@@ -4,13 +4,12 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 hn=$(hostname)
-logger -s -t $hn "starting live jpg upload"
+logger -s -t $hn "starting auroracam"
 source $here/config.ini > /dev/null 2>&1
 export DATADIR LOGDIR IPADDRESS NIGHTGAIN
 export LAT LON ALT UPLOADLOC CAMID
 
 source ~/vAuroracam/bin/activate
-#export PYTHONPATH=~/source/RMS
 
 pids=$(ps -ef | grep ${here}/grabImage | egrep -v "grep|$$" | awk '{print $2}')
 [ "$pids" != "" ] && kill -9 $pids
