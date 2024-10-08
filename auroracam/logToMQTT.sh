59efba1f255ee25aa8b9285f7a5685ded03ad86c
Mark McIntyre <markmcintyre99@googlemail.com>
2024-10-08 16:34:36 +0100

    Move meteorcam to a new location (#60)
    * A great many changes (#54) (#55)
    
    * remove local aws key file
    move setExpo to this repo
    
    * document the new function
    
    * changes to remove dependency on hardcoded key
    
    * improvements in local processes
    
    * bugfix: don't attempt to make an archive of rejects if there are none
    
    * container-based k8s testing support
    
    * bugfix in yt upload tests
    
    * upgrade lambdas to python 3.11
    
    * more robust way to generate a missed timelapse
    
    * remove unused stuff
    
    * make sure sendToMQ works even without RMS
    
    * add password to MQ server
    
    * add policy and user for allsky uploads
    
    * support for new allsky  camera
    
    * consolidating non-ukmon Terraform
    
    * renaming radio folder
    
    * improve secret hygiene
    
    * add keograms to allsky data
    
    * add gitguardian precommit support
    
    * close #51 upload allsky to youtube
    
    * document deployment and use of ansible vault
    
    * tweak to quiet AWS uploads
    
    * write logs somewhere writeable
    
    * properly obtain remote indexes for webserver
    and push any changes back
    
    * monitor allsky status = closes #53
    
    * bugfix in createCameraIndex
    
    * check allsky status and restart if needed
    
    * bugfixes in createCameraIndex
    
    * add support to send auroracam to youtube
    
    * minor improvements to my toolset
    
    * add keogram to webpage
    
    * script to get logs from server
    
    * add cpu temp to mq for auroracam
    
    * Tidying up the non-analysis scripts
    
    * better monitoring of the allsky
    
    * small syntax warning in teh auroracam code
    
    * support to reupload timelapse
    
    * bugfix for auroracam - was creating two night folders
    
    * improvements to camera backup
    
    * minor change to fix fireball sync
    
    * pause between publishing diskspace and cpu temp
    
    * updates to avoid trackstack issues
    
    * bugfixes for trackstack and daily stack if no data available
    
    * Small improvements to auroracam software
    
    * separate out the MQ settings and add support for sftp upload
    
    * improvements and simplifications
    
    * improve readme
    
    * fixing merge issues
    
    * fix errors
    
    * fixing some issues
    
    * remove dependency on meteortools
    
    * various improvements in auroracam
    
    * minor tweaks
    
    * fix line-ends
    
    * small tweak
    
    * More bugfixes
    
    * improving logging, error handling and adding cleardown of files-to-upload
    
    * lock paho-mqtt version to avoid error
    
    * small bugfixes and support to change cam network settings
    
    * monitor the openhab server with MQ
    
    * rename folder
    
    * monitor other pis
    
    * bugfix in checker script
    
    * Minor tweaks to the logging and readme
    
    * moved auroracam to its own repo
    
    * remove disused files

--- auroracam/logToMQTT.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (C) Mark McIntyre
-#
-
-here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source ~/vAuroracam/bin/activate
-
-cd $here
-python -c "from auroraCam import sendToMQTT ; sendToMQTT() ; "
