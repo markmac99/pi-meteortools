6e5f33ec60db4c92f5386a06474b5a822fa56803
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-03-27 12:43:56 +0000

    update allsky to latest build and capture my patches

--- allsky/ytUpload.py
+++ allsky/ytUpload.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os
+import sys
 import pickle
 import datetime
 from time import sleep
@@ -16,6 +17,7 @@ from google.auth.transport.requests import Request
 from googleapiclient.errors import HttpError
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
+MINS = 5
 
 
 def uploadToYoutube(here, title, fname):
@@ -40,12 +42,12 @@ def uploadToYoutube(here, title, fname):
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:# Get credentials and create an API client
-          flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            client_secrets_file, scopes)
-          credentials = flow.run_console()
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file, scopes)
+            credentials = flow.run_console()
         # Save the credentials for the next run
         with open(tokfile, 'wb') as token:
-          pickle.dump(credentials, token)
+            pickle.dump(credentials, token)
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
@@ -53,84 +55,82 @@ def uploadToYoutube(here, title, fname):
     request = youtube.videos().insert(
         part="snippet,status",
         body={
-          "snippet": {
-            "categoryId": "22",
-            "description": "Allsky camera timelapse",
-            "title": title
-          },
-          "status": {
-            "privacyStatus": "public"
-          }
+            "snippet": {
+                "categoryId": "22",
+                "description": "Allsky camera timelapse",
+                "title": title
+            },
+            "status": {
+                "privacyStatus": "public"
+            }
         },
         
-        media_body=MediaFileUpload(fname,  chunksize=-1, resumable=True)
+        media_body=MediaFileUpload(fname, chunksize=-1, resumable=True)
     )
     try: 
-      status, response = request.next_chunk() # request.execute()
-      print(status, response)
-      if response is not None:
-        if 'id' in response:
-          print(f'Video id  {response["id"]} was successfully uploaded.')
-          return True
-        else:
-          print(f'The upload failed with an unexpected response: {response}')
-          return False
+        status,response = request.next_chunk() # request.execute()
+        print(status, response)
+        if response is not None:
+            if 'id' in response:
+                print(f'Video id  {response["id"]} was successfully uploaded.')
+                return True
+            else:
+                print(f'The upload failed with an unexpected response: {response}')
+                return False
     except HttpError as e:
-       print(f'HTTP error {e.resp.status} arose with message: {e.content}')
-       return False
+        print(f'HTTP error {e.resp.status} arose with message: {e.content}')
+        return False
 
 
 def updateCrontab(here, offset=30, lati=51.88, longi=-1.31, elev=80):
-  batchname = 'youtubeUploader'
-  obs = ephem.Observer()
-  obs.lat = float(lati) / 57.3 # convert to radians, close enough for this
-  obs.lon = float(longi) / 57.3
-  obs.elev = float(elev)
-  obs.horizon = -6.0 / 57.3 # degrees below horizon for darkness
+    obs = ephem.Observer()
+    obs.lat = float(lati) / 57.3 # convert to radians, close enough for this
+    obs.lon = float(longi) / 57.3
+    obs.elev = float(elev)
+    obs.horizon = -6.0 / 57.3 # degrees below horizon for darkness
+    
+    sun = ephem.Sun()
+    rise = obs.next_rising(sun).datetime()
+    starttime = rise + datetime.timedelta(minutes=-offset)
+    print('Setting batch start time to', starttime.strftime('%H:%M'))
 
-  sun = ephem.Sun()
-  rise = obs.next_rising(sun).datetime()
-  starttime = rise + datetime.timedelta(minutes=-offset)
-  print('Setting batch start time to', starttime.strftime('%H:%M'))
-
-  cron = CronTab(user=True)
-  for job in cron:
-    if 'youtubeUploader.sh' in job.command:
-        cron.remove(job)
-        cron.write()
-  job = cron.new(f'{here}/youtubeUploader.sh > /home/allsky/logs/ytupload.log 2>&1')
-  job.setall(starttime.minute, starttime.hour, '*', '*', '*')
-  cron.write()
-  return
+    cron = CronTab(user=True)
+    for job in cron:
+        if 'youtubeUploader.sh' in job.command:
+            cron.remove(job)
+            cron.write()
+    job = cron.new(f'{here}/youtubeUploader.sh > $HOME/logs/ytupload.log 2>&1')
+    job.setall(starttime.minute, starttime.hour, '*', '*', '*')
+    cron.write()
+    return
 
 
 def scanLogFile(logname, toddt):
-  lis = open(logname,'r').readlines()
-  tllines = [x for x in lis if 'Timelapse complete' in x]
-  tllines = [x for x in tllines if toddt.strftime('%Y-%m-%d') in x]
-  if len(tllines) > 0:
-     return True
-  return False
+    lis = open(logname,'r').readlines()
+    tllines = [x for x in lis if 'Timelapse complete' in x]
+    tllines = [x for x in tllines if toddt.strftime('%Y-%m-%d') in x]
+    if len(tllines) > 0:
+        return True
+    return False
 
-MINS = 5
 
 if __name__ == "__main__":
-  today = datetime.datetime.now()
-  here = os.path.split(os.path.abspath(__file__))[0]
-  logfile = sys.argv[1]
-  allskyhome = sys.argv[2]
-  yest = today + datetime.timedelta(days=-1)
-  title = f'Allsky timelapse for {yest.strftime("%Y-%m-%d")}'
-  fname = f'allsky-{yest.strftime("%Y%m%d")}.mp4'
-  fname = os.path.join(allskyhome, 'images', f'{yest.strftime("%Y%m%d")}', fname)
-  found = False
-  counter = 0
-  print(f'waiting for {fname}')
-  while not found and counter < MINS*36:
-    found = scanLogFile(logfile, today)
-    if found:
-      print(f'   uploading {fname}')
-      uploadToYoutube(here, title, fname)
-      break
-    sleep(MINS*60)
-  updateCrontab(here)
+    today = datetime.datetime.now()
+    here = os.path.split(os.path.abspath(__file__))[0]
+    logfile = sys.argv[1]
+    allskyhome = sys.argv[2]
+    yest = today + datetime.timedelta(days=-1)
+    title = f'Allsky timelapse for {yest.strftime("%Y-%m-%d")}'
+    fname = f'allsky-{yest.strftime("%Y%m%d")}.mp4'
+    fname = os.path.join(allskyhome, 'images', f'{yest.strftime("%Y%m%d")}', fname)
+    found = False
+    counter = 0
+    print(f'waiting for {fname}')
+    while not found and counter < MINS*36:
+        found = scanLogFile(logfile, today)
+        if found:
+            print(f'   uploading {fname}')
+            uploadToYoutube(here, title, fname)
+            break
+        sleep(MINS*60)
+    updateCrontab(here)
