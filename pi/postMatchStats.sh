764384ceaea1844b88e74395b2ef6cc10d482209
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-07-24 17:15:12 +0100

    minor improvements to multicam support

--- pi/postMatchStats.sh
+++ pi/postMatchStats.sh
@@ -6,4 +6,5 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vRMS/bin/activate
 cd $here
-python -c "from sendToMQTT import sendMatchdataToMqtt;sendMatchdataToMqtt();"
+statid=$1
+python -c "from sendToMQTT import sendMatchdataToMqtt;sendMatchdataToMqtt('$statid');"
