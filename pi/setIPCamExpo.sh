9eb78d55eeb914ee75904c44fa976136738ba7c8
Mark McIntyre <markmcintyre99@googlemail.com>
2025-08-20 16:51:03 +0100

    remove nightgain as its done in the camera_settings json file now

--- pi/setIPCamExpo.sh
+++ pi/setIPCamExpo.sh
@@ -8,9 +8,9 @@ source $HOME/vRMS/bin/activate
 source $here/config.ini >/dev/null 2>&1
 
 if [ $# -lt 2 ] ; then
-  echo "usage setIPCamExpo.sh day|night camid"
+  echo "usage setIPCamExpo.sh day|night"
   exit 1
 fi
 
-logger  -s -t setIPCamExpo "Setting exposure for $1. Camera is $2, gain is $NIGHTGAIN"
-python3 $here/setExpo.py $1 $2 $NIGHTGAIN 
+logger  -s -t setIPCamExpo "Setting exposure for $1
+python3 $here/setExpo.py $1
