53dac225eeaea115da90392a30bdbf3a21ed4ee0
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-10-30 16:18:48 +0000

    new script to reprocess and reupload a set of data

---
+++ pi/reprocessCam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source ~/vRMS/bin/activate
+
+camid=$1
+dtval=$2
+if [[ "$camid" == "" || "$dtval" == "" ]] ; then 
+	echo usage: reprocessCam.sh camid dtstr
+	exit
+fi
+capdircnt=$(ls -1d ~/RMS_data/${camid}/CapturedFiles/${camid}_${dtval}* | wc -l)
+if [ $capdircnt -gt 1 ]; then
+	echo "more than one matching folder found, please refine the date"
+	echo $capdir
+	exit
+fi 
+capdir=$(ls -1d ~/RMS_data/${camid}/CapturedFiles/${camid}_${dtval}*)
+arcdir=${capdir/Capture/Archive}
+echo ""
+if [[ "$capdir" == "" || "$arcdir" == "" ]] ; then 
+	echo data not found for $camid and $dtval
+	exit
+fi 
+python -m RMS.Reprocess -c ~/source/Stations/${camid}/.config  ${capdir}
+python -m RMS.RunExternalScript -c ~/source/Stations/${camid}/.config  ${capdir} ${arcdir}
