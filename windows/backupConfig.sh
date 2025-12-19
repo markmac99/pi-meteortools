59034d56215d24c0c20604d6ef7d8994aa70e2e1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-12-19 13:47:00 +0000

    tidy up windows scripts

--- windows/backupConfig.sh
+++ windows/backupConfig.sh
@@ -3,24 +3,23 @@
 
 # backup everything
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-pushd $here
-cd /mnt/c/Users/$USER/OneDrive/dev/meteorcam_config
+pushd /mnt/c/Users/$USER/OneDrive/dev/meteorhunting/meteorcam_config
 
-rsync -avz --include-from ./rms.txt --exclude "*" uk0006:source/Stations/UK0006/ ./uk0006
-rsync -avz --include-from ./rms.txt --exclude "*" uk000f:source/Stations/UK000F/ ./uk000f
+rsync -avz --include-from ./rms.txt --exclude "*"   :source/Stations/UK0006/ ./uk0006
+rsync -avz --include-from ./rms.txt --exclude "*" ubunturms:source/Stations/UK000F/ ./uk000f
 rsync -avz --include-from ./rms.txt --exclude "*" uk001l:source/Stations/UK001L/ ./uk001l
-rsync -avz --include-from ./rms.txt --exclude "*" uk002f:source/Stations/UK002F/ ./uk002f
-rsync -avz  uk0006:.ssh/ ./uk0006/ssh
-rsync -avz  uk000f:.ssh/ ./uk000f/ssh
+rsync -avz --include-from ./rms.txt --exclude "*" ubunturms:source/Stations/UK002F/ ./uk002f
+rsync -avz  ubunturms:.ssh/ ./uk0006/ssh
+rsync -avz  ubunturms:.ssh/ ./uk000f/ssh
 rsync -avz  uk001l:.ssh/ ./uk001l/ssh
-rsync -avz  uk002f:.ssh/ ./uk002f/ssh
-rsync -avz --include-from ./ukmon.txt --exclude "*" uk0006:source/ukmon-pitools-UK0006/ ./uk0006/ukmon
-rsync -avz --include-from ./ukmon.txt --exclude "*" uk000f:source/ukmon-pitools-UK0006/ ./uk000f/ukmon
+rsync -avz  ubunturms:.ssh/ ./uk002f/ssh
+rsync -avz --include-from ./ukmon.txt --exclude "*" ubunturms:source/ukmon-pitools-UK0006/ ./uk0006/ukmon
+rsync -avz --include-from ./ukmon.txt --exclude "*" ubunturms:source/ukmon-pitools-UK0006/ ./uk000f/ukmon
 rsync -avz --include-from ./ukmon.txt --exclude "*" uk001l:source/ukmon-pitools-UK001L/ ./uk001l/ukmon
-rsync -avz --include-from ./ukmon.txt --exclude "*" uk002f:source/ukmon-pitools-UK002F/ ./uk002f/ukmon
+rsync -avz --include-from ./ukmon.txt --exclude "*" ubunturms:source/ukmon-pitools-UK002F/ ./uk002f/ukmon
 
-rsync -avz --include-from ./tackley.txt --exclude "*" uk0006:source/tackley-tools/ ./uk0006/tackley
-rsync -avz --include-from ./tackley.txt --exclude "*" uk000f:source/tackley-tools/ ./uk000f/tackley
+rsync -avz --include-from ./tackley.txt --exclude "*" ubunturms:source/tackley-tools/ ./uk0006/tackley
+rsync -avz --include-from ./tackley.txt --exclude "*" ubunturms:source/tackley-tools/ ./uk000f/tackley
 rsync -avz --include-from ./tackley.txt --exclude "*" uk001l:source/tackley-tools/ ./uk001l/tackley
-rsync -avz --include-from ./tackley.txt --exclude "*" uk002f:source/tackley-tools/ ./uk002f/tackley
+rsync -avz --include-from ./tackley.txt --exclude "*" ubunturms:source/tackley-tools/ ./uk002f/tackley
 popd
