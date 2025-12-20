0958bf119b091ac851f6b48944bb421e9dbddeb1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-12-20 00:21:42 +0000

    simplify radio image copier

--- wpserver/getLatestRadioJpgs.sh
+++ wpserver/getLatestRadioJpgs.sh
@@ -5,11 +5,8 @@ source $here/config.ini
 if [ ! -f /tmp/getLatestRadioJpgs.running ] ; then 
 	echo "1" > /tmp/getLatestRadioJpgs.running
 	cd $DATADIR/Radio
-	#aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "*latest.jpg" --include "screenshot*.jpg"
-	aws s3 cp s3://mjmm-data/Radio/RMOB_latest.jpg .
-	aws s3 cp s3://mjmm-data/Radio/3months_latest.jpg .
-	aws s3 cp s3://mjmm-data/Radio/screenshot1.jpg .
-	aws s3 cp s3://mjmm-data/Radio/screenshot2.jpg .
+	aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "sc*.png"
+	aws s3 sync s3://mjmm-data/Radio/ . --exclude "*" --include "*.jpg"
 	rm -f /tmp/getLatestRadioJpgs.running
 else
 	echo "already running"
