1c95cae5d7a67cac28a422ecda37f8b73e628097
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-12-24 07:29:19 +0000

    improved gathering of interesting radio data

--- windows/analyseRadioData.ps1
+++ windows/analyseRadioData.ps1
@@ -11,19 +11,26 @@ bash -c "rsync -avz --delete radiopi:radar_data/Captures/ ./radar_data/Captures/
 bash -c "rsync -avz --delete radiopi:radar_data/Logs/ ./radar_data/Logs/"
 
 conda activate vMeteorRadio
-$env:mrdatadir="$radarloc"
+$env:mrdatadir="$radarloc/radar_data"
 $flist=(Get-ChildItem .\radar_data\Captures\ -r )
 foreach ($i in $flist) {
+    $fullname = $i.fullname
     if (test-path $i -pathtype leaf)
     {
         $name = $i.name 
         $imgname=$name.replace('Captures','Images').replace('SMP','PSD').replace('npz','png')
         $imgname=".\radar_data\Images\" + $imgname
         if (test-path $imgname -pathtype leaf){
-            Write-Output "skipping $i"
+            Write-Output "skipping creation of $i"
         }else {
-            python C:\dev\meteorhunting\MeteorRadio\src\analyse_detection.py -s -n -3 --colour PuBu -a $i.fullname
+            python C:\dev\meteorhunting\MeteorRadio\src\analyse_detection.py -s -n -3 --colour PuBu -a $fullname
         }
     }
+}
+$flist=(Get-ChildItem .\radar_data\Captures\)
+foreach ($i in $flist) {
+    $fullname = $i.fullname
+    Write-Output $fullname
+    python C:\dev\meteorhunting\pi-meteortools\radio\getStats.py $fullname
 }
 set-location $here
