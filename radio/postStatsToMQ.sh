3fdbcd538d7b2f514eb7d79ad59420af5ed79ec6
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-01-20 14:03:41 +0000

    add datestamp to radiopi metrics

--- radio/postStatsToMQ.sh
+++ radio/postStatsToMQ.sh
@@ -12,5 +12,7 @@ currhr=$(date +%Y-%m-%d,%H)
 lastcsv=$(ls -1 $LOGDIR/20*.csv | tail -1)
 dailyct=$(egrep "$currdt" $lastcsv | wc -l)
 hourlyct=$(egrep "$currhr" $lastcsv | wc -l)
+now=$(date +%Y-%m-%dT%H:%M:%SZ)
 /usr/bin/mosquitto_pub -h $BROKER -u $USERNAME -P $PASSWORD -t meteorcams/radiopi/hourly -m $hourlyct
 /usr/bin/mosquitto_pub -h $BROKER -u $USERNAME -P $PASSWORD -t meteorcams/radiopi/daily -m $dailyct
+/usr/bin/mosquitto_pub -h $BROKER -u $USERNAME -P $PASSWORD -t meteorcams/radiopi/lastupdate -m $now
